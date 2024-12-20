5e89e9a877060d56adcf2fb71ebc947dda264dd2
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-12-20 14:37:59 +0200

    [~] Sync with [me_Uart].

--- examples/me_SegmentProcessor/me_SegmentProcessor.ino
+++ examples/me_SegmentProcessor/me_SegmentProcessor.ino
@@ -2,7 +2,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-12-18
+  Last mod.: 2024-12-20
 */
 
 /*
@@ -13,11 +13,11 @@
   are more universal than that.
 */
 
+#include <me_SegmentProcessor.h>
+
 #include <me_BaseTypes.h>
 #include <me_MemorySegment.h>
-#include <me_SegmentProcessor.h>
 #include <me_Uart.h>
-#include <me_UartSpeeds.h>
 #include <me_Console.h>
 #include <me_ProgramMemory.h>
 #include <me_WorkMemory.h>
@@ -62,7 +62,7 @@ void RunTest()
 
 void setup()
 {
-  me_Uart::Init(me_UartSpeeds::Bps_115k);
+  me_Uart::Init(me_Uart::Speed_115k_Bps);
   Console.Print("Hello there!"); // yes, Console can print memory without us
   RunTest();
   Console.Print("Done.");
